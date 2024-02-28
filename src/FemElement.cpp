b3418b143ffc5a735b2aa1e10f55d5cbfb4f562c
Andrea Di Antonio <diantonioandrea@icloud.com>
2024-02-28 11:47:30 +0100

    Confict comment

--- src/FemElement.cpp
+++ src/FemElement.cpp
@@ -4,6 +4,7 @@ apsc::Fem1d::FemElement1D::LocalMatrix
 apsc::Fem1d::FemElement1D::mass(QuadRule const & quadRule) const
 {
   using namespace apsc::Fem1d;
+  // A different comment
   FemElement1D::LocalMatrix m{nodes_.size(), nodes_.size()};
   for(std::size_t i = 0u; i < nodes_.size(); ++i)
     {
